f3dfa56e0274c82521429a5042f83b1164e3aaa1
Takayuki-Y5991 <konkon.gitsune628@gmail.com>
2026-02-15 22:59:37 +0900

    :tada: upgrade rops

--- project.clj
+++ project.clj
@@ -1,15 +1,12 @@
-(defproject org.clojars.konkon/railway-clj "0.1.0"
-  :description "Railway Oriented Programming library for Clojure with async support, circuit breaker, and retry mechanisms"
+(defproject org.clojars.konkon/railway-clj "0.2.0"
+  :description "Minimal Railway Oriented Programming for Clojure"
   :url "https://github.com/Takayuki-Y5991/railway-clj"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
-                 [org.clojure/core.async "1.7.701"]
-                 [org.clojure/tools.logging "1.3.0"]
-                 [org.clojure/spec.alpha "0.3.218"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]
-                              :plugins [[lein-cloverage "1.2.4"]]}}
-  :cloverage {:fail-threshold 70}
+                   :plugins [[lein-cloverage "1.2.4"]]}}
+  :cloverage {:fail-threshold 90}
   :min-lein-version "2.0.0"
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :sign-releases false}]]
--- test/railway_clj/core_test.clj
+++ test/railway_clj/core_test.clj
@@ -1,173 +1,158 @@
 (ns railway-clj.core-test
   (:require
-   [railway-clj.core :refer [!> <|> >-< ->error ->value attempt delay-railway either failure failure? guard lazy? success success? validate |+ |-| |>]]
-   [clojure.spec.alpha :as s]
-   [clojure.test :refer  [deftest is testing]]))
+   [railway-clj.core :refer [ok err ok? err? unwrap then recover branch or-else pipeline ->result]]
+   [clojure.test :refer [deftest is testing]]))
 
-(s/def ::name string?)
-(s/def ::age pos-int?)
-(s/def ::email (s/and string? #(re-matches #".*@.*\..*" %)))
-(s/def ::person (s/keys :req-un [::name ::age ::email]))
+;; ---------------------------------------------------------------------------
+;; 2.1 ok, err creation
+;; ---------------------------------------------------------------------------
+(deftest ok-test
+  (testing "ok returns [:ok value]"
+    (is (= [:ok 42] (ok 42)))
+    (is (= [:ok nil] (ok nil)))
+    (is (= [:ok "hello"] (ok "hello")))
+    (is (= [:ok {:a 1}] (ok {:a 1})))))
 
-(deftest result-type-test
-  (testing "Success and Failure types"
-    (let [s (success "value")
-          f (failure "error")]
-      (is (success? s))
-      (is (not (success? f)))
-      (is (failure? f))
-      (is (not (failure? s)))
-      (is (= "value" (:value s)))
-      (is (= "error" (:error f))))))
+(deftest err-test
+  (testing "err returns [:err reason]"
+    (is (= [:err "not found"] (err "not found")))
+    (is (= [:err {:type :validation :msg "invalid"}]
+           (err {:type :validation :msg "invalid"})))
+    (is (= [:err nil] (err nil)))))
 
-(deftest thread-success-test
-  (testing "|> threads success values"
-    (let [inc-fn (fn [x] (inc x))
-          double-fn (fn [x] (* 2 x))
-          result (|> (success 5) inc-fn double-fn)]
-      (is (success? result))
-      (is (= 12 (:value result)))))
+;; ---------------------------------------------------------------------------
+;; 2.2 ok?, err? predicates
+;; ---------------------------------------------------------------------------
+(deftest ok?-test
+  (testing "ok? returns true for [:ok ...]"
+    (is (true? (ok? (ok 1))))
+    (is (true? (ok? (ok nil)))))
+  (testing "ok? returns false for [:err ...]"
+    (is (false? (ok? (err "fail"))))))
 
-  (testing "|> passes failures through unchanged"
-    (let [inc-fn (fn [x] (inc x))
-          error {:message "Error"}
-          result (|> (failure error) inc-fn)]
-      (is (failure? result))
-      (is (= error (:error result))))))
+(deftest err?-test
+  (testing "err? returns true for [:err ...]"
+    (is (true? (err? (err "fail"))))
+    (is (true? (err? (err nil)))))
+  (testing "err? returns false for [:ok ...]"
+    (is (false? (err? (ok 1))))))
 
-(deftest thread-error-test
-  (testing "|-| threads error values"
-    (let [add-info (fn [e] (assoc e :info "Additional info"))
-          add-timestamp (fn [e] (assoc e :timestamp "now"))
-          error {:message "Error"}
-          result (|-| (failure error) add-info add-timestamp)]
-      (is (failure? result))
-      (is (= "Additional info" (get-in result [:error :info])))
-      (is (= "now" (get-in result [:error :timestamp])))))
+;; ---------------------------------------------------------------------------
+;; 2.3 unwrap
+;; ---------------------------------------------------------------------------
+(deftest unwrap-test
+  (testing "unwrap extracts value from ok"
+    (is (= 42 (unwrap (ok 42)))))
+  (testing "unwrap extracts reason from err"
+    (is (= "fail" (unwrap (err "fail")))))
+  (testing "unwrap handles nil in both ok and err"
+    (is (nil? (unwrap (ok nil))))
+    (is (nil? (unwrap (err nil))))))
 
-  (testing "|-| passes successes through unchanged"
-    (let [add-info (fn [e] (assoc e :info "Additional info"))
-          result (|-| (success 42) add-info)]
-      (is (success? result))
-      (is (= 42 (:value result))))))
+;; ---------------------------------------------------------------------------
+;; 2.4 then
+;; ---------------------------------------------------------------------------
+(deftest then-test
+  (testing "applies f to success value"
+    (is (= (ok 6) (then (ok 5) inc))))
+  (testing "passes failure through unchanged"
+    (is (= (err "fail") (then (err "fail") inc))))
+  (testing "uses result as-is when f returns a result"
+    (is (= (err "too big")
+           (then (ok 10) #(if (> % 5) (err "too big") (ok %))))))
+  (testing "wraps plain value in ok"
+    (is (= (ok "hello!") (then (ok "hello") #(str % "!"))))))
 
+;; ---------------------------------------------------------------------------
+;; 2.5 recover
+;; ---------------------------------------------------------------------------
+(deftest recover-test
+  (testing "applies f to failure reason"
+    (is (= (ok "recovered")
+           (recover (err "fail") (fn [_] (ok "recovered"))))))
+  (testing "passes success through unchanged"
+    (is (= (ok 42) (recover (ok 42) (fn [_] (ok "recovered")))))))
+
+;; ---------------------------------------------------------------------------
+;; 2.6 branch
+;; ---------------------------------------------------------------------------
 (deftest branch-test
-  (testing ">-< branches on success"
-    (let [success-fn (fn [v] (str "Success: " v))
-          failure-fn (fn [e] (str "Failure: " e))
-          result1 (>-< (success "good") success-fn failure-fn)
-          result2 (>-< (failure "bad") success-fn failure-fn)]
-      (is (= "Success: good" result1))
-      (is (= "Failure: bad" result2)))))
+  (testing "calls on-ok for success"
+    (is (= "value: 42"
+           (branch (ok 42)
+                   #(str "value: " %)
+                   #(str "error: " %)))))
+  (testing "calls on-err for failure"
+    (is (= "error: not found"
+           (branch (err "not found")
+                   #(str "value: " %)
+                   #(str "error: " %))))))
 
-(deftest alternative-test
-  (testing "<|> provides alternative on failure"
-    (let [result1 (<|> (success 1) (success 2))
-          result2 (<|> (failure "error") (success 2))]
-      (is (success? result1))
-      (is (= 1 (:value result1)))
-      (is (success? result2))
-      (is (= 2 (:value result2))))))
+;; ---------------------------------------------------------------------------
+;; 2.7 or-else
+;; ---------------------------------------------------------------------------
+(deftest or-else-test
+  (testing "returns alternative on failure"
+    (is (= (ok 0) (or-else (err "fail") (ok 0)))))
+  (testing "passes success through"
+    (is (= (ok 42) (or-else (ok 42) (ok 0))))))
 
-(deftest validate-test
-  (testing "validate creates success for valid data"
-    (let [valid-person {:name "John" :age 30 :email "john@example.com"}
-          result (validate ::person valid-person)]
-      (is (success? result))
-      (is (= valid-person (:value result)))))
+;; ---------------------------------------------------------------------------
+;; 2.8 pipeline
+;; ---------------------------------------------------------------------------
+(deftest pipeline-test
+  (testing "chains functions on success"
+    (let [process (pipeline inc #(* % 2))]
+      (is (= (ok 12) (process 5)))))
+  (testing "short-circuits on failure"
+    (let [fail-if-big #(if (> % 5) (err "too big") %)
+          process (pipeline inc fail-if-big #(* % 2))]
+      (is (= (err "too big") (process 5)))
+      (is (= (ok 6) (process 2)))))
+  (testing "composed function is reusable"
+    (let [process (pipeline inc #(* % 2))]
+      (is (= (ok 12) (process 5)))
+      (is (= (ok 6) (process 2))))))
 
-  (testing "validate creates failure for invalid data"
-    (let [invalid-person {:name "John" :age -30 :email "not-an-email"}
-          result (validate ::person invalid-person)]
-      (is (failure? result))
-      (is (string? (get-in result [:error :details]))))))
+;; ---------------------------------------------------------------------------
+;; 2.9 ->result
+;; ---------------------------------------------------------------------------
+(deftest ->result-test
+  (testing "returns [:ok value] on success"
+    (is (= (ok 3) (->result (+ 1 2)))))
+  (testing "returns [:err ...] on exception"
+    (let [result (->result (/ 1 0))]
+      (is (err? result))
+      (is (= :exception (:type (unwrap result))))
+      (is (string? (:message (unwrap result)))))))
 
-(deftest try-railway-test
-  (testing "!> catches exceptions and returns them as failures"
-    (let [result1 (!> (+ 1 2))
-          result2 (!> (throw (Exception. "Test exception")))]
-      (is (success? result1))
-      (is (= 3 (:value result1)))
-      (is (failure? result2))
-      (is (= "Unexpected error" (get-in result2 [:error :error])))
-      (is (= "Test exception" (get-in result2 [:error :details]))))))
+;; ---------------------------------------------------------------------------
+;; 2.10 -> + then inline pipeline
+;; ---------------------------------------------------------------------------
+(deftest threading-with-then-test
+  (testing "-> threading with then"
+    (is (= (ok 12)
+           (-> (ok 5)
+               (then inc)
+               (then #(* % 2))))))
+  (testing "short-circuits on failure in threading"
+    (is (= (err "negative")
+           (-> (ok -1)
+               (then #(if (neg? %) (err "negative") %))
+               (then inc))))))
 
-(deftest chain-test
-  (testing "|+ chains functions together"
-    (let [inc-fn (fn [x] (inc x))
-          double-fn (fn [x] (* 2 x))
-          fail-fn (fn [_] (failure {:message "Failed"}))
-          success-chain (|+ inc-fn double-fn)
-          failure-chain (|+ inc-fn fail-fn double-fn)]
-
-      (is (= 6 (:value (success-chain 2))))
-
-      (let [failure-result (failure-chain 2)]
-        (is (failure? failure-result))
-        (is (= {:message "Failed"} (:error failure-result)))))))
-
-(deftest either-test
-  (testing "either selects function based on predicate"
-    (let [even? (fn [x] (even? x))
-          double (fn [x] (* 2 x))
-          triple (fn [x] (* 3 x))
-          choose-fn (either even? double triple)]
-
-      (is (= 4 (choose-fn 2)))
-      (is (= 9 (choose-fn 3))))))
-
-(deftest guard-test
-  (testing "guard creates success for valid conditions"
-    (let [positive? (fn [x] (> x 0))
-          check-positive (guard positive? "Value must be positive")]
-
-      (is (success? (check-positive 5)))
-      (is (= 5 (:value (check-positive 5))))
-
-      (let [result (check-positive -5)]
-        (is (failure? result))
-        (is (= "Value must be positive" (get-in result [:error :error])))))))
-
-(deftest attempt-test
-  (testing "attempt catches exceptions in functions"
-    (let [risky-fn (fn [x] (/ 10 x))
-          handle-error (fn [e] (failure {:message (.getMessage e)}))
-          safe-fn (attempt risky-fn handle-error)]
-
-      (is (= 5 (safe-fn 2)))
-
-      (let [result (safe-fn 0)]
-        (is (failure? result))
-        (is (= "Divide by zero" (get-in result [:error :message]))))))
-
-(deftest delay-railway-test
-  (testing "delay-railway creates lazy railway result based on evaluation"
-    (let [eval-count (atom 0)
-          success-fn (fn []
-                       (swap! eval-count inc)
-                       (success "delayed success"))
-          failure-fn (fn []
-                       (swap! eval-count inc)
-                       (failure "delayed failure"))
-          lazy-success-result (delay-railway (success-fn))
-          lazy-failure-result (delay-railway (failure-fn))]
-
-      ;; Initially, functions should not be evaluated
-      (is (= 0 @eval-count))
-
-      ;; Force evaluation by calling the returned function
-      (let [success-lazy (lazy-success-result)
-            failure-lazy (lazy-failure-result)]
-        
-        ;; Now functions should be evaluated
-        (is (= 2 @eval-count))
-        
-        ;; Check that lazy results are created correctly
-        (is (lazy? success-lazy))
-        (is (lazy? failure-lazy))
-        (is (success? success-lazy))
-        (is (failure? failure-lazy))
-        
-        ;; Force evaluation to get actual values
-        (is (= "delayed success" (->value success-lazy)))
-        (is (= "delayed failure" (->error failure-lazy))))))))
+;; ---------------------------------------------------------------------------
+;; Alias tests
+;; ---------------------------------------------------------------------------
+(deftest alias-test
+  (testing ">> is the same as then"
+    (is (= (ok 6) (railway-clj.core/>> (ok 5) inc))))
+  (testing "<< is the same as recover"
+    (is (= (ok "fixed") (railway-clj.core/<< (err "fail") (fn [_] (ok "fixed"))))))
+  (testing ">< is the same as branch"
+    (is (= "ok:42" (railway-clj.core/>< (ok 42) #(str "ok:" %) #(str "err:" %)))))
+  (testing "|? is the same as or-else"
+    (is (= (ok 0) (railway-clj.core/|? (err "fail") (ok 0)))))
+  (testing ">>> is the same as pipeline"
+    (let [process (railway-clj.core/>>> inc #(* % 2))]
+      (is (= (ok 12) (process 5))))))
